cd8226e3fee42ce0e6060997c4b2a0289c4a98d9
SealCHK <kospryshin@gmail.com>
2022-11-29 21:54:40 +0600

    1

--- PAPIRUS_WPF/Elements/Output.xaml.cs
+++ PAPIRUS_WPF/Elements/Output.xaml.cs
@@ -20,9 +20,48 @@ namespace PAPIRUS_WPF.Elements
     /// </summary>
     public partial class Output : UserControl
     {
+        /// <summary>
+        /// Called when the state of the output is changed
+        /// </summary>
+        public event StateChangeHandler StateChange;
+        public delegate void StateChangeHandler();
+
+        // The state of the output
+        private bool _state;
+
+        /// <summary>
+        /// The current state of the Output
+        /// </summary>
+        public bool State
+        {
+            get
+            {
+                return _state;
+            }
+            set
+            {
+                _state = value;
+            }
+        }
+
+        /// <summary>
+        /// Creates a new output class
+        /// </summary>
         public Output()
         {
+            _state = false;
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Updates any connected inputs. Called once per tick.
+        /// </summary>
+        public void CallChange()
+        {
+            if (StateChange != null)
+            {
+                StateChange();
+            }
         }
     }
 }
--- PAPIRUS_WPF/MainWindow.xaml.cs
+++ PAPIRUS_WPF/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PAPIRUS_WPF.Elements;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,18 +22,76 @@ namespace PAPIRUS_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+
+
+        //The boolean that signifys when an output is being linked
+        private bool _linkingStarted = false;
+        //The temporary line that shows when linking an output
+        private LineGeometry _tempLink;
+        //The output that is being linked to
+        private Output _tempOutput;
+
         public MainWindow()
         {
             InitializeComponent();
 
+            CircuitCanvas.MouseDown += CircuitCanvas_MouseDown;
+          // CircuitCanvas.MouseMove += CircuitCanvas_MouseMove;
+           // CircuitCanvas.MouseUp += CircuitCanvas_MouseUp;
+          //  CircuitCanvas.Drop += CircuitCanvas_Drop;
         }
-
-        public double Zoom
+        private void CircuitCanvas_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            get
+            //Get the position of the mouse relative to the circuit canvas
+            Point MousePosition = e.GetPosition(CircuitCanvas);
+
+            //Do a hit test under the mouse position
+            HitTestResult result = VisualTreeHelper.HitTest(CircuitCanvas, MousePosition);
+
+            //Make sure that there is something under the mouse
+            if (result == null || result.VisualHit == null)
+                return;
+
+            //If the mouse has hit a border
+            if (result.VisualHit is Border)
             {
-                return zoom.Value;
+                //Get the parent class of the border
+                Border border = (Border)result.VisualHit;
+                var IO = border.Parent;
+
+                //If the parent class is an Output
+                if (IO is Output)
+                {
+                    //Cast to output
+                    Output IOOutput = (Output)IO;
+
+                    //Get the center of the output relative to the canvas
+                    Point position = IOOutput.TransformToAncestor(CircuitCanvas).Transform(new Point(IOOutput.ActualWidth / 2, IOOutput.ActualHeight / 2));
+
+                    //Creates a new line
+                    _linkingStarted = true;
+                    _tempLink = new LineGeometry(position, position);
+
+                    //Assign it to the list of connections to be displayed
+                    Connections.Children.Add(_tempLink);
+
+                    //Assign the temporary output to the current output
+                    _tempOutput = (Output)IO;
+
+                    e.Handled = true;
+                }
             }
         }
+        private void CircuitCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            //If there is a linking in progress
+            if (_linkingStarted)
+            {
+                //Move the link endpoint to the current location of the mouse
+                _tempLink.EndPoint = e.GetPosition(CircuitCanvas);
+                e.Handled = true;
+            }
+        }
+
     }
 }
